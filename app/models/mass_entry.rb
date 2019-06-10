549dbf6573c60c993f986397e216b7639e7c15da
Dileep Nandanam <dileep@Dileeps-MacBook-Air.local>
2019-06-09 17:53:03 -0700

    font

--- app/models/mass_entry.rb
+++ app/models/mass_entry.rb
@@ -5,7 +5,7 @@ class MassEntry
         name = Nokogiri::HTML(Net::HTTP.get_response(URI.parse(url)).response.body).css('strong')[0].text
         tags = Nokogiri::HTML(Net::HTTP.get_response(URI.parse(url)).response.body).css('div.video-tags').css('a').map(&:text).join(' ')
       elsif url.include? 'youtube.com'
-      	name = Nokogiri::HTML(Net::HTTP.get_response(URI.parse(url)).response.body).css('title')[0]
+      	name = Nokogiri::HTML(Net::HTTP.get_response(URI.parse(url)).response.body).css('title')[0].text
       	tags = ''
       end
       Link.create(url: url, name: name, tags: tags)
