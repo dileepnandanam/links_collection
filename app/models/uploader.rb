53a06fd0be9733cfaa1c17c6d0b087aba09ea0ac
Dileep Nandanam <dileep@Dileeps-MacBook-Air.local>
2019-06-12 09:09:28 -0700

    css

--- app/models/uploader.rb
+++ app/models/uploader.rb
@@ -1,9 +1,15 @@
 class Uploader
 	def self.upload
-		urls = Dir.entries('/Users/Dileep/xlinks_folder').select{ |f| f.include? "link-" }.map{|f| 
-			File.open("/Users/Dileep/xlinks_folder/#{f}").read.strip
-		}.join(',')
+		Dir.entries('/Users/Dileep/xlinks_folder').select{ |f| f.include? "link-" }.each_slice(4).each{ |chunk|
+			urls = chunk.map {|f|
+				File.open("/Users/Dileep/xlinks_folder/#{f}").read.strip
+			}.join(',')
+			Net::HTTP.post_form(URI.parse("http://xlinks.herokuapp.com/links"), {'link[url]' => urls})
+		}
+		
+		Dir.entries('/Users/Dileep/xlinks_folder').select{ |f| f.include? "link-" }.map{|f| 
 
-		Net::HTTP.post_form(URI.parse("http://xlinks.herokuapp.com/links"), {'link[url]' => urls})
+			File.delete "/Users/Dileep/xlinks_folder/#{f}"
+		}
 	end
 end
