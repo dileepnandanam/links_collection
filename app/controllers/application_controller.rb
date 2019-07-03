3e8ada5a80ea82270fbbc829dd982d1ad4f5b802
Dileep Nandanam <dileep@Dileeps-MacBook-Air.local>
2019-07-03 18:39:27 +0530

    adspace

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,3 @@
 class ApplicationController < ActionController::Base
-  before_action :redirect_to_affiliated_site
 
-  def redirect_to_affiliated_site
-    url = SiteSetting.where(name: 'affiliate_url').first
-    if url.present?
-      return redirect_to url.value
-    end
-  end
 end
