94cdfdc0e8407601825d04b4de51658adddc83d5
Dileep Nandanam <dileepsnandanam@gmail.com>
2019-09-13 04:50:45 +0530

    upload stat

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -132,6 +132,12 @@ class LinksController < ApplicationController
   def lkjhgertyjnbvftyh
     render json: {
       date: Date.today,
+      counts: {
+        gay: Link.search_count('gay', 'gay'),
+        lesbian: Link.search_count('lesb', 'lesb'),
+        total: Link.count,
+        uploaded_today: Link.where(updated_at: (1.days.ago..Time.now)).count
+      },
       visitors: Visitor.where(created_at: (1.days.ago..Time.now)).group(:ip).count.length,
       all_time_visitors: Visitor.group(:ip).count.length,
       total_searches: Query.where(created_at: (1.days.ago..Time.now)).count,
