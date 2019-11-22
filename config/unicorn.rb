f0651b5e123e3e39bd2103eb719eba753dd635a3
Dileep Nandanam <dileepsnandanam@gmail.com>
2019-11-22 15:38:32 +0530

    multiple unicorn socets

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -11,7 +11,7 @@ preload_app true
 timeout 30
 
 # Set up socket location
-listen "#{shared_dir}/sockets/unicorn.sock", :backlog => 64
+listen "#{shared_dir}/sockets/unicorn_links_collevtion.sock", :backlog => 64
 
 # Logging
 stderr_path "#{shared_dir}/log/unicorn.stderr.log"
